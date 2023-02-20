fe025e70bb69df43272e1dc940f9da1219d9d1ca
rahul <nirmal.rahul1@gmail.com>
2023-02-20 11:29:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Header from "./components/Header";
 
 function App() {
+
+  useEffect(()=>{
+    console.log("useEffect Called");
+  })
+
+  console.log("Function body");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Header />
+    {console.log("Inside JSX")}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+<div className="header">
+      <h1>Use Effect</h1>
+      </div>
+  )
+}
+
+export default Header
